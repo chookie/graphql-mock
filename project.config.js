07e0354a8d066a24be23b625f7c708e136068d13
Alison Johnston <alison@johnston.name>
2017-02-09 00:35:29 +0000

    Update project.config.js

--- project.config.js
+++ project.config.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const mongoUser = 'rgrjs';
-const mongoPwd = '9996896728';
+const mongoUser = 'XXXX';
+const mongoPwd = 'XXXX';
 
 export default {
   port: process.env.port || 3003,
-  mongo: process.env.MONGO_URL || `mongodb://${mongoUser}:${mongoPwd}@ds019766.mlab.com:19766/rgrjs`
+  mongo: process.env.MONGO_URL || `mongodb://${mongoUser}:${mongoPwd}@ds012345.mlab.com:1234/rgrjs`
 }
